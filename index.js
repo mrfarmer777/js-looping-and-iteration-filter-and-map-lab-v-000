e9bbfe39efff6ede9fa347bb7446fea5e92a9de6
Matt Farmer <mrfarmer777@gmail.com>
2018-07-27 02:59:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,11 @@ function driversWithRevenueOver(drvs,revenue){
   return drvs.filter(function (drv){
     return drv.revenue>revenue;
   });
-  
+}
+
+
+function driverNamesWithRevenueOver(drvs,revenue){
+  return driversWithRevenueOver(drvs,revenue).map(function(drv){
+    return drv.name;
+  });
 }
