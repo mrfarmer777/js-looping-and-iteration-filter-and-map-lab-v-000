3e022761b0e4bcfb55843839210eac4218644902
Matt Farmer <mrfarmer777@gmail.com>
2018-07-27 02:57:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution here:
+//drvs is an array of objects
+function driversWithRevenueOver(drvs,revenue){
+  return drvs.filter(function (drv){
+    return drv.revenue>revenue;
+  });
+  
+}
